f1a4ee6d04dafe3c3cf650147fc5ad32cb02d4ed
Christian Blank <christian@colorful-projects.de>
2017-01-10 05:45:26 +0100

    Add exact value type
    This solves #6

---
+++ spec/Type/ExactValueTypeSpec.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace spec\StructureCheck\Type;
+
+use StructureCheck\Type\ExactValueType;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class ExactValueTypeSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->beConstructedWith(null);
+        $this->shouldHaveType(ExactValueType::class);
+    }
+
+    function it_is_valid_for_null_if_null_is_the_value()
+    {
+        $this->beConstructedWith(null);
+        $this->check(null)->isValid()->shouldBe(true);
+    }
+}
---
+++ src/Type/ExactValueType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace StructureCheck\Type;
+
+use StructureCheck\Result;
+use StructureCheck\ResultInterface;
+
+class ExactValueType
+{
+
+    /**
+     * @var string
+     */
+    private static $errorMessage = 'The value %s is not the same value as %s.';
+
+    /**
+     * ExactValueType constructor.
+     *
+     * @param mixed $value
+     */
+    public function __construct($value)
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * @param mixed $value
+     *
+     * @return ResultInterface
+     */
+    public function check($value)
+    {
+        $checkResult = $this->value === $value;
+
+        return new Result(
+            $checkResult,
+            !$checkResult ? [sprintf(self::$errorMessage, json_encode($value), $this->value)] : []
+        );
+    }
+}
