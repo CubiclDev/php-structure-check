3172a323c68df6c753f4a347d2ceb539e8fe651e
Christian Blank <christian@colorful-projects.de>
2017-01-15 07:45:56 +0100

    3 datetime type (#12)
    * Add datetime type
    
    * Update type list in readme

---
+++ spec/Type/DatetimeTypeSpec.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace spec\StructureCheck\Type;
+
+use StructureCheck\Type\DatetimeType;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class DatetimeTypeSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->beConstructedWith('d-m-Y h:m:s', 'UTC');
+        $this->shouldHaveType(DatetimeType::class);
+    }
+
+    function it_should_return_valid_for_correct_values()
+    {
+        $this->beConstructedWith('d-m-Y h:m:s', 'Europe/Berlin');
+
+        $this->check('12-12-2012 12:12:10')->isValid()->shouldBe(true);
+    }
+
+    function it_should_return_invalid_for_others()
+    {
+        $this->beConstructedWith('d-m-Y h:m:s', 'Europe/Berlin');
+
+        $this->check(null)->isValid()->shouldBe(false);
+        $this->check('foo')->isValid()->shouldBe(false);
+        $this->check([])->isValid()->shouldBe(false);
+        $this->check(1.234)->isValid()->shouldBe(false);
+        $this->check(true)->isValid()->shouldBe(false);
+        $this->check(false)->isValid()->shouldBe(false);
+    }
+}
