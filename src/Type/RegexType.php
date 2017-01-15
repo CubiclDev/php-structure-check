55c69262d35608a35cf6256e1caec4611b61fe35
Christian Blank <mail@cblank.de>
2017-01-15 07:49:39 +0100

    Add regex type

---
+++ src/Type/RegexType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace StructureCheck\Type;
+
+use StructureCheck\Result;
+use StructureCheck\ResultInterface;
+
+/**
+ * Class RegexType
+ * @package StructureCheck\Type
+ */
+class RegexType
+{
+    /**
+     * @var string
+     */
+    private static $errorMessage = 'The value %s does not match the regex %s';
+
+    /**
+     * @var string
+     */
+    private $regex;
+
+    /**
+     * RegexType constructor.
+     *
+     * @param string $regex
+     */
+    public function __construct($regex)
+    {
+        $this->regex = $regex;
+    }
+
+    /**
+     * @param mixed $value
+     *
+     * @return ResultInterface
+     */
+    public function check($value)
+    {
+        $checkResult = is_string($value) && preg_match($this->regex, $value) === 1;
+
+        return new Result(
+            $checkResult,
+            !$checkResult ? [sprintf(self::$errorMessage, json_encode($value), $this->regex)] : []
+        );
+    }
+}
