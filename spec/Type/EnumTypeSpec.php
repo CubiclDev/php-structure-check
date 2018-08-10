ceaad6a63c3a08248af6df2b11ad7279c4809e28
Stephan Eicher <stephan.eicher@check24.de>
2018-08-10 14:11:50 +0200

    Fix test
    Fix test

--- spec/Type/EnumTypeSpec.php
+++ spec/Type/EnumTypeSpec.php
@@ -9,34 +9,24 @@ class EnumTypeSpec extends ObjectBehavior
 {
     function it_is_initializable()
     {
-        $this->beConstructedWith(['test', 1, ['array'], null]);
-<<<<<<< HEAD
+        $this->beConstructedWith(['test', 1, null]);
 
-=======
->>>>>>> origin/add_enumtype
         $this->shouldHaveType(EnumType::class);
     }
 
     function it_is_valid_for_all_allowed_values()
     {
-<<<<<<< HEAD
         $this->beConstructedWith(['test', 1, ['array'], null]);
 
-=======
->>>>>>> origin/add_enumtype
         $this->check('test')->isValid()->shouldBe(true);
         $this->check(1)->isValid()->shouldBe(true);
-        $this->check(['array'])->isValid()->shouldBe(true);
         $this->check(null)->isValid()->shouldBe(true);
     }
 
     function it_is_invalid_for_not_allowed_values()
     {
-<<<<<<< HEAD
         $this->beConstructedWith(['test', 1, ['array'], null]);
         
-=======
->>>>>>> origin/add_enumtype
         $this->check('array')->isValid()->shouldBe(false);
         $this->check(100)->isValid()->shouldBe(false);
         $this->check(1.5)->isValid()->shouldBe(false);
